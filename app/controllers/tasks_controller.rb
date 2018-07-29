09e5d4a36f47cc9be185e3f58bc5fd2e2deee1b6
Elias Seichter <esai@posteo.org>
2018-07-29 22:06:33 +0200

    extracting the create-form into a generic partial, for use in new and edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,16 +7,34 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def edit
+    puts params[:id]
+    @task = Task.find(params[:id])
+  end
+
   def new
     @task = Task.new
   end
 
   def create
-    @task = Task.new post_params
+    puts 'create ese'
+    puts post_params
+    @task = Task.new(post_params)
     @task.save
+    redirect_to(:action => 'index')
+  end
+
+  def update
+    puts 'update ese'
+    @task = Task.find(params[:id])
+    if @task.update(post_params)
+      redirect_back()
+    else
+      render 'edit'
+    end
   end
 
   private def post_params
-    params.require(:task).permit(:description, :is_done)
+    params.require(:task).permit(:description, :is_done, :id)
   end
 end
